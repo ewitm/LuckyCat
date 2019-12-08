2a09f4041941d9199770d585b1d1dcfcf3e59d32
ewitm <ewitmer@wisc.edu>
2019-12-07 20:51:20 -0600

    Initial Commit
    initcommit 12/7/19

---
+++ Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Elise Witmer
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
